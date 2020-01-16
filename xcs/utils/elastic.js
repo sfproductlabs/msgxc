65c11648f23560ffa79dc8c5daa8392a79571458
Andrew Grosser <dioptre@gmail.com>
2020-01-15 21:18:32 -0800

    fixed elastic

--- xcs/utils/elastic.js
+++ xcs/utils/elastic.js
@@ -31,57 +31,69 @@ if (process.env.CASSANDRA_VERIFY_SERVER) {
 const client = new elasticsearch.Client(opts);
 
 
-async function query(query = '',  options = {}) {
-  
-    const qopts = {
-        ...{
-            index: process.env.ELASTIC_INDEX_DEFAULT,
-            type: process.env.ELASTIC_TYPE_DEFUALT,
-            size: Number(process.env.ELASTIC_SIZE_DEFUALT || 100),
-            from: 0
-        }, ...options
+async function query(query = '', options) {
+
+    let qopts = {
+        index: process.env.ELASTIC_INDEX_DEFAULT,
+        type: process.env.ELASTIC_TYPE_DEFUALT,
+        size: Number(process.env.ELASTIC_SIZE_DEFUALT || 100),
+        from: 0,
+        must_not: ''
     };
 
-    query = query.replace(/-/g, ' ');
-    query = query.replace(/@/g, ' @ ');
-    //https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-query-string-query.html#_reserved_characters
-    query = query.replace(/\+|\-|\=|\&|\||\>|\<|\!|\(|\)|\{|\}|\[|\]|\^|\"|\~|\?|\:|\\|\//g, '');
-  
-
-  let mustNotQuery = '';
-  
-
-  return await client.search({
-    index:qopts.index,
-    type: qopts.type,
-    from: qopts.from,
-    size: qopts.size,
-    body: {
-      query: {
-        bool: {
-          must: {
-            query_string : {
-              query,
-              default_operator: 'AND',
-              analyze_wildcard: true
-            }
-          },
-          must_not: {
-            query_string : {
-              query: mustNotQuery
-            }
-          }
+    if (typeof query === 'string') {
+        if (typeof options === 'object') {
+            qopts = {...qopts, ...options}
         }
-      },
-      sort: [
-        (qopts.sort || { updated: 'desc' })
-      ]
+        query = query.replace(/-/g, ' ');
+        query = query.replace(/@/g, ' @ ');
+        //https://www.elastic.co/guide/en/elasticsearch/reference/current/query-dsl-query-string-query.html#_reserved_characters
+        query = query.replace(/\+|\-|\=|\&|\||\>|\<|\!|\(|\)|\{|\}|\[|\]|\^|\"|\~|\?|\:|\\|\//g, '');
+
+        return await client.search({
+            index: qopts.index,
+            type: qopts.type,
+            from: qopts.from,
+            size: qopts.size,
+            body: {
+                query: {
+                    bool: {
+                        must: {
+                            query_string: {
+                                query,
+                                default_operator: 'AND',
+                                analyze_wildcard: true
+                            }
+                        },
+                        must_not: {
+                            query_string: {
+                                query: qopts.must_not
+                            }
+                        }
+                    }
+                },
+                sort: [
+                    (qopts.sort || { updated: 'desc' })
+                ]
+            }
+        });
+    } else if (typeof query === 'object') {
+        if (query.body) {
+            return await client.search({ ...qopts, ...query});
+        } else {
+            qopts = {...qopts, ...options || {}}
+            qopts.body = qopts.body || {};
+            qopts.body.query = { ...(qopts.body.query || {}), ...query };
+            return await client.search( qopts );
+        }
+        
+    } else {
+        return null;
     }
-  })
 }
 
 
 module.exports = {
-  client,
-  query
+    client,
+    query
 }
