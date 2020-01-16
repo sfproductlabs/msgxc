8c74414be9105e0338f96f96a30034db3e2663fd
Andrew Grosser <dioptre@gmail.com>
2020-01-15 19:54:08 -0800

    fixed elastic in 6+

--- xcs/.setup/schema/cassandra/elastic.1.sh
+++ xcs/.setup/schema/cassandra/elastic.1.sh
@@ -1,52 +1,67 @@
 #!/bin/bash
 
 # setup
-# curl -w "\n" -k -XDELETE "http://localhost:9200/msgxc"
-curl -w "\n" -k -XPUT "http://localhost:9200/msgxc"
+# 
+curl -w "\n" -k -XDELETE "http://localhost:9200/mxctriage"
+#curl -w "\n" -k -XDELETE "http://localhost:9200/mxccerts"
+curl -w "\n" -k -XDELETE "http://localhost:9200/mxcfailures"
 
 # mxctriage - completed
-curl -w "\n" -k -H 'Content-Type: application/json' -XPUT "http://localhost:9200/msgxc/_mapping/updates" -d '{
-  "updates": {
-    "properties" : {
-      "id":              { "type": "keyword", "index": true, "cql_collection": "singleton" }
+curl -w "\n" -k -H 'Content-Type: application/json'  -XPUT  "http://localhost:9200/mxctriage/" -d '{
+    "settings" : { "keyspace" : "msgxc" },
+    "mappings": {
+        "mxctriage": {
+            "properties" : {
+                "mxctid": { "type": "keyword", "index": true, "cql_collection": "singleton" },
+                "completed": { "type": "date", "index": true, "cql_collection": "singleton" },
+                "data": { "type": "keyword", "index": true, "cql_collection": "singleton" }
+            }
+        }
     }
-  }
 }'
 
-curl -w "\n" -k -H 'Content-Type: application/json'  -XPUT  "http://localhost:9200/msgxc/_mapping/mxccerts" -d '{
-  "mxccerts": {
-    "properties" : {
-      "certid":              { "type": "keyword", "index": true, "cql_collection": "singleton" }
+# curl -w "\n" -k -H 'Content-Type: application/json'  -XPUT  "http://localhost:9200/mxccerts" -d '{
+#     "settings" : { "keyspace" : "msgxc" },
+#     "mappings": {
+#         "mxccerts" : { "discover" : ".*" }
+#     }
+# }'
+
+# "cql_struct": "frozen<map>",
+curl -w "\n" -k -H 'Content-Type: application/json'  -XPUT  "http://localhost:9200/mxcfailures/" -d '{
+    "settings" : { "keyspace" : "msgxc" },
+    "mappings": {
+        "mxcfailures": {
+            "properties" : {
+                "mxctid": { "type": "keyword", "index": true, "cql_collection": "singleton" },
+                "died": { "type": "date", "index": true, "cql_collection": "singleton" },
+                "retries": { "type": "integer", "index": true, "cql_collection": "singleton" },
+                "mdevice": {
+                    "type": "nested",
+                    "include_in_parent": true,
+                    "dynamic": "false",
+                    "cql_collection": "singleton",
+                    "cql_udt_name": "mxcdevice",
+                    "properties": {
+                        "mtype":    { "type": "keyword", "index": true,  "cql_collection": "singleton" },
+                        "did":      { "type": "keyword", "index": true,  "cql_collection": "singleton" },
+                        "updated":  { "type": "date", "index": true, "cql_collection": "singleton" }
+                    }
+                }
+            }
+        }
     }
-  }
 }'
 
-#   "preview_image_url": { "type": "keyword", "index": false, "cql_collection": "singleton" },
-    #   "expert_slugs":      { "type": "keyword", "index": true, "cql_collection": "set" },
-    #   "categories":        { "type": "keyword", "index": true, "cql_collection": "set" },
-    #   "published":         { "type": "date",    "index": true, "cql_collection": "singleton" },
-    #   "slug":              { "type": "keyword", "index": true, "cql_collection": "singleton", "normalizer": "custom_sort_normalizer" },
-    #   "description":       { "type": "keyword", "index": true, "cql_collection": "singleton" },
-    #   "short_description": { "type": "keyword", "index": true, "cql_collection": "singleton" },
-    #   "visibility":        { "type": "keyword", "index": true, "cql_collection": "singleton" },
-    #   "sections": {
-    #     "type": "nested",
-    #     "include_in_parent": true,
-    #     "dynamic": "false",
-    #     "cql_struct": "frozen<map>",
-    #     "cql_collection": "singleton",
-    #     "cql_udt_name": "file",
-    #     "properties": {
-    #       "title":    { "type": "keyword", "index": true,  "cql_collection": "singleton" },
-    #       "subtitle": { "type": "keyword", "index": true,  "cql_collection": "singleton" },
-    #       "name":     { "type": "keyword", "index": false, "cql_collection": "singleton" },
-    #       "type":     { "type": "keyword", "index": false, "cql_collection": "singleton" }
-    #     }
-    #   },
-    #   "product_id":        { "type": "keyword", "index": false, "cql_collection": "singleton" }
+nodetool rebuild_index msgxc mxctriage elastic_mxctriage_idx
+#nodetool rebuild_index msgxc mxccerts elastic_mxccerts_idx
+nodetool rebuild_index msgxc mxcfailures elastic_mxcfailures_idx
 
 # Test
-curl -XGET -H 'Content-Type: application/json' "http://localhost:9200/msgxc/mxctriage/_search?pretty" -d '
+#curl -XGET -H -k http://localhost:9200/mxctriage/mxctriage/_search?pretty=true&q=*:*
+
+
+curl -XGET -H 'Content-Type: application/json' "http://localhost:9200/mxctriage/_search?pretty" -d '
 {
   "query": {
     "bool":{
@@ -61,13 +76,8 @@ curl -XGET -H 'Content-Type: application/json' "http://localhost:9200/msgxc/mxct
   }
 }'
 
-# mxcfailures - died
-curl -XPUT -H 'Content-Type: application/json' "http://localhost:9200/msgxc/_mapping/mxcfailures" -d '{
-        "discover" : ".*"    
-}'
 
-# Test
-curl -XGET -H 'Content-Type: application/json' "http://localhost:9200/msgxc/mxcfailed/_search?pretty" -d '
+curl -XGET -H 'Content-Type: application/json' "http://localhost:9200/mxcfailures/_search?pretty" -d '
 {
   "query": {
     "bool":{
@@ -83,5 +93,3 @@ curl -XGET -H 'Content-Type: application/json' "http://localhost:9200/msgxc/mxcf
 }'
 
 
-nodetool rebuild_index msgxc mxctriage elastic_mxctriage_idx
-nodetool rebuild_index msgxc mxcfailures elastic_mxcfailures_idx
