5d7099011bfc30483341ca6b320d754e0baaa049
Andrew Grosser <dioptre@gmail.com>
2020-03-24 17:31:18 -0700

    Feature/triage (#8)
    * wip
    
    * wip
    
    * wip
    
    * pub/sub minimum almost working
    
    * updated threads
    
    * wip
    
    * wip
    
    * wip
    
    * tested 100k+ set size and elastic
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wp
    
    * wp
    
    * wip
    
    * added www
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * final v1 updates
    
    * wip
    
    * wip

--- xcs/.setup/keys/production/gencerts.sh
+++ xcs/.setup/keys/production/gencerts.sh
@@ -1,32 +1,156 @@
 #!/bin/bash
 
-#USAGE: 
-# Generate certs and root
-#./gencerts.sh ild 1 true
-# Generate only certs
-#./gencerts.sh ild 2
+# CA
+CA_DAYSVALID=30000
+
+# OTHER
+HOST_SUFFIX=sfpl.io
+STOREPASS=YInKGOL6P7kzJCx
+KEYPASS=YInKGOL6P7kzJCx
+TRUSTSTOREPASS=YInKGOL6P7kzJCx
+VALIDITY=30000
+OUTDIR=$PWD
+CA_CERT=$OUTDIR/rootCa.crt
+CA_KEY=$OUTDIR/rootCa.key
+CA_SRL=$OUTDIR/rootCa.srl
+CA_SUBJECT=/CN=rootCa/OU=PRODUCTION/O=SFPL/C=US/
 
 
-if [ -z "$1" ]
-  then
-    echo "No certificate name supplied"
-    exit 1
-fi
+#########################################################
 
-if [ -z "$2" ]
-  then
-    echo "No certificate number supplied"
-    exit 1
-fi
+function generateCa {
+  # delete old ca
+  rm -f $CA_KEY > /dev/null
+  rm -f $CA_CERT > /dev/null
+  rm -f $CA_SRL > /dev/null
 
-if [ -z "$3" ]
-  then
-    echo "Skipping CA Generation"
-  else
-    openssl genrsa -out rootCa.key 4096
-    openssl req -new -x509 -days 365 -key rootCa.key -out rootCa.crt -subj "/OU=SFPL"
-fi
+  # create new ca
+  openssl req -new -x509 -nodes \
+    -subj $CA_SUBJECT \
+    -keyout $CA_KEY \
+    -out $CA_CERT \
+    -days $CA_DAYSVALID
+}
 
-openssl genrsa -out $1$2.key 1024
-openssl req -new -key $1$2.key -out $1$2.csr -subj "/CN=$1$2"
-openssl x509 -req -days 365 -in $1$2.csr -CA rootCa.crt -CAkey rootCa.key -set_serial 01 -out $1$2.crt
+function generateModule {
+  # remove old files
+  rm -f $OUTDIR/$1.jks > /dev/null
+  rm -f $OUTDIR/$1.p12 > /dev/null
+  rm -f $OUTDIR/$1.crt > /dev/null
+  rm -f $OUTDIR/$1.key > /dev/null
+  rm -f $OUTDIR/$1.csr > /dev/null
+
+  # 1) generate keypair
+  keytool -genkeypair -keyalg RSA -alias $1 \
+    -keystore $OUTDIR/$1.jks \
+    -storepass $STOREPASS \
+    -keypass $KEYPASS \
+    -validity $VALIDITY \
+    -keysize 2048 \
+    -dname "$2"
+
+  # 2) generate cert request
+  keytool -certreq -alias $1 \
+    -keystore $OUTDIR/$1.jks \
+    -file $OUTDIR/$1.csr \
+    -keypass $KEYPASS \
+    -storepass $STOREPASS \
+    -ext $3 \
+    -dname "$2"
+
+  # 3) sign cert using ca
+  openssl x509 -req -CA $CA_CERT -CAkey $CA_KEY \
+    -in $OUTDIR/$1.csr \
+    -out $OUTDIR/$1.crt \
+    -days $VALIDITY \
+    -CAcreateserial \
+    -CAserial $CA_SRL
+
+  # 4) add rootCa to store
+  keytool -importcert -alias rootCa \
+    -keystore $OUTDIR/$1.jks \
+    -file $CA_CERT \
+    -noprompt \
+    -keypass $KEYPASS \
+    -storepass $STOREPASS
+
+  # 5) import signed certificate
+  keytool -importcert -alias $1 \
+    -keystore $OUTDIR/$1.jks \
+    -file $OUTDIR/$1.crt \
+    -noprompt \
+    -keypass $KEYPASS \
+    -storepass $STOREPASS
+
+  # 6) create pkcs12 variant of keystore
+  keytool -importkeystore \
+    -srcalias $1 \
+    -srckeystore $OUTDIR/$1.jks \
+    -destkeystore $OUTDIR/$1.p12 \
+    -deststoretype PKCS12 \
+    -keypass $KEYPASS \
+    -storepass $STOREPASS \
+    -srcstorepass $STOREPASS
+
+  # 7) extract private-key from keystore
+  openssl pkcs12 -nocerts \
+    -in $OUTDIR/$1.p12 \
+    -out $OUTDIR/$1.key \
+    -passin pass:$STOREPASS \
+    -passout pass:$STOREPASS
+
+  # 8) save private-key unencrypted
+  openssl rsa -in $OUTDIR/$1.key -out $OUTDIR/$1.key -passin pass:$KEYPASS
+}
+
+function generateTrust {
+  # remove old files
+  rm -f $OUTDIR/$1.jks > /dev/null
+
+  # 1) add rootCa to truststore
+  keytool -importcert -alias rootCa \
+    -keystore $OUTDIR/$1.jks \
+    -file $CA_CERT \
+    -noprompt \
+    -keypass $KEYPASS \
+    -storepass $TRUSTSTOREPASS
+
+  # 2) import signed client certificate
+  keytool -importcert -alias $2 \
+    -keystore $OUTDIR/$1.jks \
+    -file $OUTDIR/$2.crt \
+    -noprompt \
+    -keypass $KEYPASS \
+    -storepass $TRUSTSTOREPASS
+}
+
+#ca
+generateCa
+
+#nginx
+generateModule nginx \
+  "CN=NGINX, OU=PRODUCTION, O=SFPL, C=US" \
+  san=dns:api.$HOST_SUFFIX,dns:tr.$HOST_SUFFIX,dns:sso.$HOST_SUFFIX,dns:xcs.$HOST_SUFFIX,dns:msgxc.$HOST_SUFFIX
+
+#cassandra
+generateModule cassandra-server \
+  "CN=CASSANDRA-SERVER, OU=PRODUCTION, O=SFPL, C=US" \
+  san=dns:cassandra1.$HOST_SUFFIX,dns:cassandra2.$HOST_SUFFIX,dns:cassandra3.$HOST_SUFFIX
+
+generateModule cassandra-client \
+  "CN=CASSANDRA-CLIENT, OU=PRODUCTION, O=SFPL, C=US" \
+  san=dns:cassandra-client.$HOST_SUFFIX
+
+generateTrust cassandra-truststore cassandra-client
+
+#nats
+generateModule nats-server \
+  "CN=NATS-SERVER, OU=PRODUCTION, O=SFPL, C=US" \
+  san=dns:nats1.$HOST_SUFFIX,dns:nats2.$HOST_SUFFIX,dns:nats3.$HOST_SUFFIX
+
+generateModule nats-client \
+  "CN=NATS-CLIENT, OU=PRODUCTION, O=SFPL, C=US" \
+  san=dns:nats-client.$HOST_SUFFIX
+
+openssl genpkey -algorithm rsa -out $OUTDIR/priv.key
+openssl rsa -in $OUTDIR/priv.key -pubout -out $OUTDIR/pub.key
