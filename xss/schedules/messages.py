6a9b90a2ad32993434207afda20e1fab626510c0
Andrew Grosser <dioptre@gmail.com>
2020-03-31 21:22:06 -0700

    wip

--- xss/schedules/messages.py
+++ xss/schedules/messages.py
@@ -2,15 +2,31 @@ import requests
 import uuid
 from elasticsearch import Elasticsearch
 from os import path, walk, getenv
+import time
 
 # TODO: Not implemented yet
-es = Elasticsearch(["localhost"], sniff_on_connection_fail=True, sniffer_timeout=60)
+es = Elasticsearch(getenv('ELASTIC_HOSTS').split(','), sniff_on_connection_fail=True, sniffer_timeout=60)
 
 def process_scheduled_messages():
     #Setup this queue id
     qid = str(uuid.uuid1())
     svc = 'xss'
-    res = es.search(index="mstore", body={"query": {"match_all": {}}})
+    now = int(round(time.time() * 1000))
+    #print("now", now) # checked: update mstore set scheduled='2020-04-01 04:14:34+0000' where tid=5ae3c890-5e55-11ea-9283-4fa18a847130 and mid=7edf6140-73cd-11ea-aaaf-3b383fad559b;
+    res = es.search(index="mstore", body={
+                'query': {
+                    'bool': {
+                        'must': [
+                            { 'exists': { 'field': "scheduled" } },
+                            { "range" : {"scheduled" : { "lte" : now, "format" : "epoch_millis" } } }
+                        ]
+                    }
+                },
+                'sort': [
+                    { 'scheduled': 'asc' }
+                ]
+            }
+    )
     print("Got %d messages:" % res['hits']['total'])
     for hit in res['hits']['hits']:
         data = {'tid' : hit["_source"]['tid'], 'mid' : hit["_source"]['mid'], 'qid' : qid, 'svc' : svc }
@@ -23,5 +39,5 @@ def process_scheduled_messages():
             }
         )
         print(repr(r.json()))
-        print("%(mid)s %(tid)s" % hit["_source"])
+        # print("%(mid)s %(tid)s" % hit["_source"])
     return res
